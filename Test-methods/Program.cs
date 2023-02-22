9b6110d87791b4ba75d9c1afe35c5cfb60be3350
Lenovo <Lenovo@DESKTOP-GDE046H>
2023-02-22 16:27:44 -0500

    Add project files.

---
+++ Test-methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_methods
+{
+public static class Triangle
+{
+        //Anandita dutta 8881602
+        public static void Main(string[] args) { }
+    public static string ValidTriangle(int firstAngle, int secondAngle, int
+    thirdAngle)
+    {
+        string result;
+        if ((firstAngle + secondAngle + thirdAngle) == 90)
+        {
+            result = "The triangle is valid.";
+        }
+        else
+        {
+            result = "The triangle is NOT valid.";
+        }
+        return result;
+    }
+}
+}
---
+++ ValidatTriange/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Test_methods;
+
+
+
+namespace ValidateTriangle
+
+{
+
+    public class TriangleTest
+
+    {
+
+        //Anandita dutta 8881602
+
+        static void Main(string[] args)
+
+        {
+
+
+
+        }
+
+
+
+        [TestFixture]
+
+
+
+        public class TriangleTest1
+
+        {
+
+            [Test]
+
+
+
+            public void ValidTriangle_Input60and60and60_OutputVaidTriangle()
+
+            {
+
+                //Arrange
+
+                int firstAngle = 60;
+
+                int secondAngle = 60;
+
+                int thirdAngle = 60;
+
+
+
+                string expected = "The triangle is valid.";
+
+
+
+                //Act
+
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+
+
+                //Assert
+
+                Assert.AreEqual(expected, actual);
+
+            }
+
+        }
+
+
+
+    }
+}
